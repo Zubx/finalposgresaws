2d606a0553cab17b26d984d8f88ac14850e79901
Zub <zub.ingco@gmail.com>
2022-08-20 11:14:43 +0700

    sabtu 20 august
    maintenance awal

--- publicpostgres.sql
+++ publicpostgres.sql
@@ -1,4 +1,29 @@
 -- Active: 1660546976939@@zubdb.cabyoqiwyaa1.us-west-1.rds.amazonaws.com@5432@postgres@rawinput
+
 ----Step1 create tabel dari seluruh penjualan
+
+--merubah data types karena sebelumnya pakai integer jadi nolak sistemnya
 alter Table product_sold 
 alter COLUMN sku TYPE TEXT
+;
+
+
+--- kalau aku mau masukin ini, system akan nolak karena produkdetail ada di schema public, bukan di rawdata input
+CREATE Table product_details (LIKE productdetails)
+;
+--- ambil list status pesanan
+create Table list_status (
+    id SERIAL     ,
+    internal_status TEXT
+);
+
+DROP TABLE list_status
+
+;
+SELECT
+distinct internal_status
+INTO list_status
+FROM product_sold
+
+
+
