f9d1f1f02a4089c983120b7f4571555bb070094a
Zub <zub.ingco@gmail.com>
2022-08-22 17:11:14 +0700

    data notes
    data ready to connect but problem found

--- revenuedata.sql
+++ revenuedata.sql
@@ -5,3 +5,7 @@
 --- sekarang test koneksi ke GDS
 --- GDS loading lama dan tidak konek ke posgres muncul error : "Sorry, we encountered an error and were unable to complete your request"
 --- sepertinya kalau pake cloud posgre harus pake SSL certificate
+--- ternyata udah ada jawabannya di stack overflow
+--- masih mencari running codenya dimana, apakah di python ?
+--- aku running di GitBash jalan zub trus di tanya tanya kan, lengkapnya aku kasih di notion ya
+--- udah muncul dooongggg, udah dipindah ke folder lain biar g keupload ke git haha
